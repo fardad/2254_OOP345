234fce878b08786c8dcee30ef8c76e4c8451d2f6
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2025-05-14 12:44:48 -0400

    sync

--- NEE/b-May12/prg.cpp
+++ NEE/b-May12/prg.cpp
@@ -1,21 +1,10 @@
 #include <iostream>
-#include "pointers.h"
 using namespace std;
-typedef int* intptr;
-
-void copyMem( void* des, const void* src, size_t size ) {
-   for ( size_t i = 0; i < size; i++ ) {
-      reinterpret_cast<char*>(des)[i] = reinterpret_cast<const char*>(src)[i];
-   }
-}
 
 int main( ) {
-   double a, b = 1234.5678;
-   int i[5], j[5]{ 1, 2, 3, 4,5 };
-   copyMem(  &a,&b,  sizeof( b ) );
-   copyMem( i, j, sizeof( int ) * 5 );
-   cout << a << endl;
-   for ( int n = 0; n < 5; cout << i[n++] << " " );
-   cout << endl;
+   int x[]{ 10, 20, 30, 40 };
+   for ( int& elem : x ) {
+      cout << elem << endl;
+   }
    return 0;
 }
